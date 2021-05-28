0857f9a5b182fa06d9346748d9f83fadc1df4efd
OSIRISDMN\NMayekiso <ndimphiwem@osiristrading.com>
2021-05-28 16:56:54 +0200

    feat (redirect): Added Route redirect for paths that don't exist

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', loadChildren: () => import('./authentication/authentication.module').then((m) => m.AuthenticationModule) },
-  { path: 'phonebooks', loadChildren: () => import('./phonebooks/phonebooks.module').then((m) => m.PhonebooksModule) }
+  { path: 'phonebooks', loadChildren: () => import('./phonebooks/phonebooks.module').then((m) => m.PhonebooksModule) },
+  { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
